42282fea54327d1e2c4f16edee2837a5a8a328eb
Júlia Miranda Rodrigues <juliamrz2@outlook.com>
2022-05-17 13:03:06 -0300

    Add 3 primeiras consultas

--- FACULDADE/atv_11_05_2022_consulta.sql
+++ FACULDADE/atv_11_05_2022_consulta.sql
@@ -2,8 +2,18 @@
 select disciplinas.cod, disciplinas.nome, cursou.semestre, cursou.nota
 from disciplinas
 inner join cursou on disciplinas.cod = cursou.disciplina
-where aluno_id = (select id from aluno where matr = matricula), cursou.id = aluno_id;
+where cursou.aluno = 2;
 
 -- Retornar lista de matr, nome e média das notas de cada aluno
+select aluno.matr, aluno.nome, AVG(cursou.nota)
+from aluno
+inner join cursou on aluno.matr = cursou.aluno
+group by aluno.nome;
+
 -- Lista de turmas e quantidade de alunos inscritos, ordenada das maiores para as menores turmas
--- Lista dos alunos (matr e nome) que não estão inscritos em nenhuma turma
+select inscrito.turma, count(inscrito.aluno) as qtd_alunos
+from inscrito
+group by inscrito.turma
+order by qtd_alunos desc;
+
+
